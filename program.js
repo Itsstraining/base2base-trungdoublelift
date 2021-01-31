6aef43ee09e226e046e889aa3d136af3058548af
NguyenHoangTrung <trungdoublelift@gmail.com>
2021-01-31 17:35:54 +0700

    lan 2

--- program.js
+++ program.js
@@ -1,56 +1,85 @@
 const { join } = require("path");
 
-let temp=[];
-let temp2=[];
-let KQ=0;
-let output=" ";
-function binToY(N,X,Y){
-  switch(Y){
-    case Y=10:
-    XToBin(N,X);
-    for(let i=0;i<temp.length;i++){
-      KQ=KQ+temp[i]*Math.pow(2,i);
-      output=" "+KQ
-    }
-    break;
-    case Y=2:
-    XToBin(N,X);
-    for(let i=0;i<temp.length;i++){
-      temp2.push(temp[temp.length-(1+i)]);
-      output=temp2.join(" ");
-    }
+let temp = [];
+let temp2 = [];
+let KQ = 0;
+let output = " ";
+let temp3 = [];
+function binToY(N, X, Y) {
+  switch (Y) {
+    case Y = 10:
+      XToBin(N, X);
+      for (let i = 0; i < temp.length; i++) {
+        KQ = KQ + temp[temp.length-(1+i)] * Math.pow(2, i);
+      }
+      output = "" + KQ
+      break;
+    case Y = 2:
+      XToBin(N, X);
+      for (let i = 0; i < temp.length; i++) {
+        temp2.push(temp[temp.length - (1 + i)]);
+        output = temp2.join("");
+      }
   }
-return output;
+  temp = [];
+  temp2 = [];
+  KQ = 0;
+  console.log(output)
 }
-function XToBin(N,X){
-  switch(X){
-    case X=10:
-      for(let i=0;;){
-        temp.push(parseInt(N%2));
-        N= N/2;
-        if(N/2<1){
-          temp.push(parseInt(N%2));
+function XToBin(N, X) {
+  switch (X) {
+    case X = 10:
+      for (let i = 0; ;) {
+        temp.push(parseInt(N % 2));
+        N = N / 2;
+        if (N / 2 < 1) {
+          temp.push(parseInt(N % 2));
           break;
         }
-        else{
+        else {
           i++
         }
       }
       break;
-
+    case X = 8:
+      let M = ""
+      M = M + N;
+  
+      temp = M.split("");
+      for (let i = 0; i < temp.length; i++) {
+        temp2.push(parseInt(temp[i]));
+      }
+      for (let i = 0; i < temp2.length; i++) {
+        if (temp2[i] == 1) {
+        }
+        else {
+          temp3.push(XToBin(temp2[i], 10).join(""));
+        }
+      }
+      temp = temp3.join("");
+      break;
+    case X = 2:
+      let Z=""
+      Z=Z+N;
+      temp = Z.split("");
+      for(let i=0;i<temp.length;i++){
+        temp2.push(parseInt(temp[i]));
+      }
+      temp=temp2;
+      break;
   }
-  return temp;
 }
 
 
 // Add new functions, variables here
 
 function main(input) {
-  let temp=input.split(" ");
-  let N=parseInt(temp[0]);
-  let X=parseInt(temp[1]);
-  let Y=parseInt(temp[2]);
-  console.log(binToY(N,X,Y));
+  let nhap = input.split(" ");
+  let N = parseInt(nhap[0]);
+  let X = parseInt(nhap[1]);
+  let Y = parseInt(nhap[2]);
+  binToY(N, X, Y);
+  // console.log(XToBin(N, X));
   // Your code goes here
   // Using console.log to answer the question
 }
